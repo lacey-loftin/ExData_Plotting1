d5052d8d6ab690a91c6e4e97c975440951bbce51
Lacey Loftin <lacey@laceyloftin.com>
2017-02-10 19:29:48 -0600

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,15 @@
+##Setting and tidying data
+householdFile <- "household_power_consumption.txt"
+  
+  plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+  
+  finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+ 
+  SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+  
+  finalData <- cbind(SetTime, finalData)
+
+ ##Plot the Histogram 
+  hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+  
+  
---
+++ plot2.R
@@ -0,0 +1,2 @@
+##Plot Global Active Power in KW
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
